24352d8b5f863f49f2d317ce2477d79b77f35235
Baldeep Singh Sidhu <726755@student.tor.loc>
2019-03-28 16:46:58 -0400

    commit at 4:46pm

---
+++ baldeep/baldeep/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baldeep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            Network.Download();
+        }
+    }
+    class Network
+    {
+        static public Task Download()
+        {
+
+            return Task.Run() => { Thread.Sleep(3000)});
+        }
+    }
+}
